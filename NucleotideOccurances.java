b9f1222e7069015b3b9a5e93ac4b233428709c9d
Sam-Serpoosh <sam.serpoosh@gmail.com>
2013-12-11 19:30:09 -0500

    Add README

--- NucleotideOccurances.java
+++ NucleotideOccurances.java
@@ -1,5 +1,3 @@
-package com.sam.nucleotidefrequency;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -10,67 +8,67 @@ import java.util.List;
 import java.util.Map;
 
 public class NucleotideOccurances {
-	private static final String ADENINE  = "A";
-	private static final String THYMINE  = "T";
-	private static final String CYTOSINE = "C";
-	private static final String GUANINE  = "G";
-	
-	private String _directory;
-	private Map<String, Integer> _nucleotidesCount;
+  private static final String ADENINE  = "A";
+  private static final String THYMINE  = "T";
+  private static final String CYTOSINE = "C";
+  private static final String GUANINE  = "G";
 
-	public NucleotideOccurances(String directory) {
-		_directory = directory;
-		_nucleotidesCount = new HashMap<String, Integer>();
-		_nucleotidesCount.put(ADENINE, 0);
-		_nucleotidesCount.put(THYMINE, 0);
-		_nucleotidesCount.put(CYTOSINE, 0);
-		_nucleotidesCount.put(GUANINE, 0);
-	}
-	
-	public Map<String, Integer> countOccurancesOfNucleotides() throws IOException {
-		List<String> fileNames = listAllFilesInDirectory();
-		for (String file : fileNames)
-			addNumberOfEachNucleotidesToItsTotal(file);
-		
-		return _nucleotidesCount;
-	}
-	
-	private List<String> listAllFilesInDirectory() { 
-		File dir = new File(_directory);
-		File[] allFiles = dir.listFiles();
-		List<String> fileNames = new ArrayList<String>();
-		for (File f : allFiles)
-			fileNames.add(f.getName());
-		
-		return fileNames;
-	}
-	
-	private void addNumberOfEachNucleotidesToItsTotal(String file) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(_directory + "/" + file));
-		String line;
-		while((line = reader.readLine()) != null)
-			addNumberOfEachNucleotideInLineToItsTotal(line);
-		reader.close();
-	}
-	
-	private void addNumberOfEachNucleotideInLineToItsTotal(String line) {
-		for (int i = 0; i < line.length(); i++) {
-			String currentChar = String.valueOf(line.charAt(i));
-			switch(currentChar) {
-				case ADENINE:
-					_nucleotidesCount.put(ADENINE, _nucleotidesCount.get(ADENINE) + 1);
-					break;
-				case THYMINE:
-					_nucleotidesCount.put(THYMINE, _nucleotidesCount.get(THYMINE) + 1);
-					break;
-				case CYTOSINE:
-					_nucleotidesCount.put(CYTOSINE, _nucleotidesCount.get(CYTOSINE) + 1);
-					break;
-				case GUANINE:
-					_nucleotidesCount.put(GUANINE, _nucleotidesCount.get(GUANINE) + 1);
-					break;
-			}
-		}
-	}
-	
+  private String _directory;
+  private Map<String, Integer> _nucleotidesCount;
+
+  public NucleotideOccurances(String directory) {
+    _directory = directory;
+    _nucleotidesCount = new HashMap<String, Integer>();
+    _nucleotidesCount.put(ADENINE, 0);
+    _nucleotidesCount.put(THYMINE, 0);
+    _nucleotidesCount.put(CYTOSINE, 0);
+    _nucleotidesCount.put(GUANINE, 0);
+  }
+
+  public Map<String, Integer> countOccurancesOfNucleotides() throws IOException {
+    List<String> fileNames = listAllFilesInDirectory();
+    for (String file : fileNames)
+      addNumberOfEachNucleotidesToItsTotal(file);
+
+    return _nucleotidesCount;
+  }
+
+  private List<String> listAllFilesInDirectory() { 
+    File dir = new File(_directory);
+    File[] allFiles = dir.listFiles();
+    List<String> fileNames = new ArrayList<String>();
+    for (File f : allFiles)
+      fileNames.add(f.getName());
+
+    return fileNames;
+  }
+
+  private void addNumberOfEachNucleotidesToItsTotal(String file) throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(_directory + "/" + file));
+    String line;
+    while((line = reader.readLine()) != null)
+      addNumberOfEachNucleotideInLineToItsTotal(line);
+    reader.close();
+  }
+
+  private void addNumberOfEachNucleotideInLineToItsTotal(String line) {
+    for (int i = 0; i < line.length(); i++) {
+      String currentChar = String.valueOf(line.charAt(i));
+      switch(currentChar) {
+        case ADENINE:
+          _nucleotidesCount.put(ADENINE, _nucleotidesCount.get(ADENINE) + 1);
+          break;
+        case THYMINE:
+          _nucleotidesCount.put(THYMINE, _nucleotidesCount.get(THYMINE) + 1);
+          break;
+        case CYTOSINE:
+          _nucleotidesCount.put(CYTOSINE, _nucleotidesCount.get(CYTOSINE) + 1);
+          break;
+        case GUANINE:
+          _nucleotidesCount.put(GUANINE, _nucleotidesCount.get(GUANINE) + 1);
+          break;
+      }
+    }
+  }
+
 }
---
+++ README.markdown
@@ -0,0 +1,33 @@
+## Frequency of Nucelotides in Human Chromosome 1
+
+The number and frequency of each of the nucleotides is as following in a chromosome 1 of homo sapiens:
+
+```
+Number of each Nucleotide:
+  Adenine  = 65570891
+  Cytosine = 47024414
+  Guanine  = 47016563
+  Thymine  = 65668756
+  
+Frequency of each Nucleotide:
+  A: 29.106%
+  C: 20.874%
+  T: 29.15%
+  G: 20.87%
+  
+```
+
+As can be noticed in the result the number and frequency of these nucleotides is **NOT** fairly distributed between all 4 nucleotides!
+
+And as we know DNA is a stranded pair of nucleotides sequence and usually in DNA A is paired with T (Thymine) and C (Cytosine) is paired with G(Guanine) and the following is the number of these pairs and their frequency again in homo sapiens chromosome 1:
+
+```
+A:T -> 131239647
+C:G -> 94040977
+
+A:T -> 58.256%
+C:G -> 41.744%
+
+```
+
+Also as you can see in the result the number of *A:T* pairs is way more than the number of *C:G* pairs!
